58302d8ac47eb8a87ff82c851d39b6e642572e78
Jiikuy <georg.rieger@eblcom.ch>
2017-05-19 21:13:20 +0200

    Added actionbar button for CalibrationActivity

--- app/src/main/java/ch/jiikuy/velocitycalculator/MainActivity.java
+++ app/src/main/java/ch/jiikuy/velocitycalculator/MainActivity.java
@@ -6,6 +6,9 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +24,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Start calibration activity
+        int id = item.getItemId();
+        if(id == R.menu.main_menu) {
+            Intent intent = new Intent(this, CalibrationActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     public void recordVideo(View view) {
+        // Capture video
         Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
         if(takeVideoIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
@@ -29,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openVideo(View view) {
+        // Open video
         Intent openVideoIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         openVideoIntent.addCategory(Intent.CATEGORY_OPENABLE);
         openVideoIntent.setType("video/*");
@@ -37,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        // Start CalculationActivity
         if(requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK || requestCode == REQUEST_ViDEO_OPEN && resultCode == RESULT_OK) {
             Uri videoUri = null;
             if(resultData != null) {
